59867fbd2c99e0aa4b5188ffcf3e49edc8f4a8de
Matth5050 <matthew.f.herbert@gmail.com>
2023-01-28 14:58:24 -0800

    added tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="text-3xl font-bold underline">
           Edit  and save to reload.
         </p>
       </header>
