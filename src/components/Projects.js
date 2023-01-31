a8d63fe9dacc8278b45e19f871e5bbadfaac1516
Matth5050 <matthew.f.herbert@gmail.com>
2023-01-30 20:40:58 -0800

    new fileds

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,7 @@ export default function Projects() {
     {
       pic: jokeBook,
       alt: "JokeBook Screen Shot",
+      stack: "React",
       title: "JokeBook",
       description:
         "React web-app designed to bring all things funny to one place. Users can click a button to receive a joke or they can view user submitted jokes and vote on who is the most funny!",
@@ -17,7 +18,8 @@ export default function Projects() {
     {
       pic: leaderBet,
       alt: "LeaderBet Screen Shot",
-      title: "leaderBet",
+      stack: "React",
+      title: "LeaderBet",
       description:
         "Sports betting app built with React that allows users to view games, scores, and odds while competing with each other for most wins on a global leader board",
       order: 4,
@@ -25,6 +27,7 @@ export default function Projects() {
     {
       pic: cardanoWatch,
       alt: "CardanoWatch Screen Shot",
+      stack: "PHP",
       title: "CardanoWatch",
       description:
         "Built to better aggregate important news and events related to the Cardano block chain",
@@ -33,6 +36,7 @@ export default function Projects() {
     {
       pic: slag,
       alt: "S.L.A.G. Screen Shot",
+      stack: "C#/.NET",
       title: "S.L.A.G. Game",
       description:
         "Multiplayer PvP battle game where players build their teams and watch them fight",
@@ -42,13 +46,16 @@ export default function Projects() {
 
   return (
     <React.Fragment>
-      <div className="mt-5 sm:flex">
+      <div className="sm:flex">
         <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
           {Object.values(projects).map((projs) => (
-            <div>
-              <img src={projs.pic} alt={projs.alt}></img>
-              <h4 className="text-lg font-bold">{projs.title}</h4>
+            <div className="mb-5">
+              <img src={projs.pic} alt={projs.alt} className="mt-5"></img>
+              <h4 className="text-lg font-bold mt-4">
+                {projs.title} - {projs.stack}
+              </h4>
               <p className="mt-1">{projs.description}</p>
+              <hr className="mt-2"></hr>
             </div>
           ))}
         </div>
@@ -56,34 +63,3 @@ export default function Projects() {
     </React.Fragment>
   );
 }
-
-// import React, { useEffect, useState } from "react";
-// import { collection, onSnapshot } from "firebase/firestore";
-// import { db } from "../firebase";
-
-// export default function Projects() {
-//   const projects = [
-//     {
-//       pic: "/Users/mherbert/Desktop/portfolio-v2/src/assets/jokeBook.png",
-//       title: "JokeBook",
-//       description:
-//         "React web-app designed to bring all things funny to one place. Users can click a button to receive a joke or they can view user submitted jokes and vote on who is the most funny!",
-//       order: 5,
-//     },
-//   ];
-
-//   return (
-//     <React.Fragment>
-//       <div className="mt-5 sm:flex">
-//         <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
-//           {Object.values(projects).map((projs) => (
-//             <div>
-//               <h4 className="text-lg font-bold">{projs.title}</h4>
-//               <p className="mt-1">{projs.description}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
